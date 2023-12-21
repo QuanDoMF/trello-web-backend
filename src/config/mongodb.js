ddd5f895c74071be6e55a4aa973848308ee5d29d
Tquandoo <quantrungdo2401@gmail.com>
2023-12-21 14:42:02 +0700

    connect_mongodb_env_variables

---
+++ src/config/mongodb.js
@@ -0,0 +1,42 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+import { MongoClient, ServerApiVersion  } from 'mongodb'
+import { env } from '~/config/environment'
+
+
+// khởi tạo một đối tượng trelloDatabaseInstance ban đầu là null (vì ta chưa connect)
+let trelloDatabaseInstance = null
+
+// khởi tạo một đối tượng mongoClientInstance để connect tới mongoDB
+const mongoClientInstance = new MongoClient (env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+}
+})
+
+// function kết nối tới databse
+export const CONNECT_DB = async () => {
+  // gọi kết nối tới mongoDB atlas với URI đã khai báo trong thân của MongoClientInstance
+  await mongoClientInstance.connect()
+
+  // kết nối thành công thì lấy database theo tên và gán ngược lại vào biến trelloDataBaseInstance
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+
+// Function GET_DB (không async) này có nhiệm vụ export ra cái database instance sau khi đã connect
+// connect thành công tới MongoDB để chúng tả sử dụng ở nhiều nơi khác nhau  trong code
+// Lưu ý phải đảm bảo chỉ luôn gọi cái getDB này sau khi đã kết nối thành công tới MongoDB
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Databse first!')
+  return trelloDatabaseInstance
+}
+
+// đóng kết nối tới DB
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
---
+++ src/server.js
@@ -0,0 +1,51 @@
+/*eslint-disable no-console */
+import express from 'express'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
+const START_SERVER = () => {
+  const app = express();
+  app.get('/', async (req, res) => {
+    res.end('<h1>Hello World </h1><hr>')
+  })
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    // eslint-disable-next-line no-console
+    console.log(`3. Hi ${env.AUTHOR}, Back-end Server is running successfully at Host: ${env.APP_HOST} and Port: ${env.APP_PORT}`)
+  })
+  exitHook(() => {
+    console.log('4. Server is shutting down...')
+    CLOSE_DB()
+      .then(() => {
+        console.log('5. Disconnected from MongoDB Cloud Atlas')
+      })
+  })
+}
+
+// cách viết 2
+// IIFE JavaScript Invoked Function
+(async () => {
+  try {
+    console.log('1. Connecting to MongoDB Cloud Atlas...')
+    await CONNECT_DB()
+    console.log('2. Connected to MongoDB Cloud Atlas!')
+
+    // khởi động server backend sau khi connect Database thành công
+    START_SERVER()
+  } catch (error) {
+    console.log(error)
+    process.exit(0) // khi lỗi thì như kiểu sẽ dừng con server lại
+  }
+})()
+
+// cách viết 1
+// console.log('1. Connecting to MongoDB Cloud Atlas...')
+// CONNECT_DB()
+//   .then(() => {
+//     console.log('2. Connected to MongoDB Cloud Atlas!')
+//   })
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.log(error)
+//     process.exit(0) // khi lỗi thì như kiểu sẽ dừng con server lại
+//   })
+
