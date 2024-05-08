8ab4196b4a8daa0b09a5fd82a6c8c824e86a9e53
tquandoo_24 <quantrungdo2401@gmail.com>
2024-05-08 11:17:25 +0700

    final

--- src/middlewares/errorHandlingMiddleware.js
+++ src/middlewares/errorHandlingMiddleware.js
@@ -9,7 +9,6 @@ import { env } from '~/config/environment'
 
 export const errorHandlingMiddleware = (err, req, res, next) => {
 
-  // Nếu dev không cẩn thận thiếu statusCode thì mặc định sẽ để code 500 INTERNAL_SERVER_ERROR
   if (!err.statusCode) err.statusCode = StatusCodes.INTERNAL_SERVER_ERROR
 
   // Tạo ra một biến responseError để kiểm soát những gì muốn trả về
